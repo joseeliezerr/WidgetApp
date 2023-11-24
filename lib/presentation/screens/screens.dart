3d7f3fbb368b972fb1127219a0be5227aa512e79
Jose Rivera <Joseeliezerr123@gmail.com>
2023-11-24 10:31:14 -0600

    Screen creadas y barril actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,12 @@
+
+export 'package:widget_app/presentation/screens/ui_constrols/ui_controls_screen.dart';
+export 'package:widget_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widget_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widget_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widget_app/presentation/screens/animated/app_tutorial/app_tutorial_screen.dart';
+export 'package:widget_app/presentation/screens/animated/animated_screen.dart';
 export 'package:flutter/material.dart';
 export 'package:widget_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widget_app/presentation/screens/home/home_screen.dart';
+
 
